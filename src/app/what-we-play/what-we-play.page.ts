a420671b7f4a3576fab8c1263dff84c6f6402459
sariippp <abdurrahmanasgf88@gmail.com>
2024-09-26 15:58:16 +0700

    what we play

--- src/app/what-we-play/what-we-play.page.ts
+++ src/app/what-we-play/what-we-play.page.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./what-we-play.page.scss'],
 })
 export class WhatWePlayPage implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
   }
 
+  games = [
+    {
+      id: 1,
+      name: 'Sea of Thieves',
+      description: 'Sea of Thieves is a smash-hit pirate adventure game, offering the quintessential pirate experience of plundering lost treasures, intense battles, vanquishing sea monsters and more. Dive in with this revised edition of the game, which includes access to digital bonus media.',
+      image: 'assets/img/seaOfThieves.jpg'
+    },
+    {
+      id: 2,
+      name: 'Dota 2',
+      description: 'Every day, millions of players worldwide enter battle as one of over a hundred Dota heroes. And no matter if its their 10th hour of play or 1,000th, theres always something new to discover. With regular updates that ensure a constant evolution of gameplay, features, and heroes, Dota 2 has taken on a life of its own.',
+      image: 'assets/img/dota2.jpg'
+    }
+  ];
 }
