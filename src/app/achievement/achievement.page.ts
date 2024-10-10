4ca3a16b732b78ea7969d33fc3516792b8483b67
alrickdemetrius <alrick.demetrius@gmail.com>
2024-10-10 16:41:15 +0700

    achivement selesai

--- src/app/achievement/achievement.page.ts
+++ src/app/achievement/achievement.page.ts
@@ -9,8 +9,13 @@ import { GamesService } from '../games.service';
 })
 export class AchievementPage implements OnInit {
   index = 0
-  game_list :any[]=[]
-  constructor(private route: ActivatedRoute,private games : GamesService) { }
+  game_list: any[] = []
+  selectedYear: string = 'All';
+  availableYears: number[] = [];
+  filteredAchievements: any[] = [];
+  allAchievements: any[] = [];
+
+  constructor(private route: ActivatedRoute, private games: GamesService) { }
 
   ngOnInit() {
     this.route.params.subscribe(
@@ -19,6 +24,32 @@ export class AchievementPage implements OnInit {
       }
     )
     this.game_list = this.games.games
+    this.loadAchievements();
   }
 
+  loadAchievements() {
+    this.allAchievements = [];
+    
+    const game = this.game_list[this.index];
+
+    game.achievements.forEach((achievement: any) => {
+      this.allAchievements.push(achievement);
+      if (!this.availableYears.includes(achievement.year)) {
+        this.availableYears.push(achievement.year);
+      }
+    });
+
+    this.availableYears.sort((a, b) => b - a);
+    this.filterAchievements();
+  }
+
+  filterAchievements() {
+    if (this.selectedYear === 'All') {
+      this.filteredAchievements = this.allAchievements;
+    } else {
+      this.filteredAchievements = this.allAchievements.filter(
+        achievement => achievement.year === parseInt(this.selectedYear)
+      );
+    }
+  }
 }
